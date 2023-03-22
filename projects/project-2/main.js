92b261d27502e395a0fb74de3e687a662d171f56
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-03-22 15:16:31 -0400

    renaming titles

--- projects/project-2/main.js
+++ projects/project-2/main.js
@@ -1,3 +1,4 @@
+
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
@@ -10,36 +11,22 @@ function processMorse( data ){
 
   data.forEach( function(item, index){
     
-      let newItem = document.createElement("div");
-      newItem.classList.add("icon");
-
+    
+  let newItem = document.createElement("div");
+  newItem.classList.add("icon");
 
       newItem.innerHTML = `
       <div class="alphabet">${item.alphabet}</div>
       <div class="detail">
         <div class="alphabet">${item.alphabet}</div>
-        <audio src="morse_code_audios/${item.sound}">
-    </div>
+        <audio src="morse_code_audios/${item.sound}" controls>
+      </div>
       `;
       container.appendChild(newItem); 
       newItem.addEventListener('click', function(){
-        newItem.classList.toggle("active");
-      });
-      let trigger = document.getElementById('trigger'); //get element by id attribute
-      let target = document.querySelector('.target'); //get element by CSS seelector
-      trigger.addEventListener('click', function(){
-          target.classList.toggle('effect');
+        newItem.item.alphabet.toggle("active");
       });
       
-      // if using jQuery, the above code will look like
-      // make sure you are loading jQuery in your <head> with a script tag
-      // uncomment the CDN link
-      
-      // $(document).ready(function(){
-      //     $('#trigger').click(function(){
-      //         $('.target').toggleClass('effect');
-      //     })
-      // });
       
   });
 
