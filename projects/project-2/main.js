84f07463a4a41d3564927ae6ac47fec6436fb679
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-03-23 22:09:09 -0400

    push clicking for each

--- projects/project-2/main.js
+++ projects/project-2/main.js
@@ -11,14 +11,11 @@ function processMorse( data ){
 
   data.forEach( function(item, index){
     
-    let buttonb=document.getElementById("B");
-    buttonb.addEventListener('click', function(){
-
-      
 
 
   let newItem = document.createElement("div");
-  newItem.classList.add("icon");
+    newItem.classList.add("icon");
+    newItem.classList.add(`letter-${item.alphabet}`);
 
       newItem.innerHTML = `
       <div class="alphabet">${item.alphabet}</div>
@@ -31,11 +28,16 @@ function processMorse( data ){
       `;
       container.appendChild(newItem); 
       // newItem.item.alphabet.toggle("active");
-      });
-      
   });
-  
-
-
 
 }
+
+let buttons = document.querySelectorAll('.button');
+buttons.forEach( element=>{
+  element.addEventListener('click', function(){
+      console.log('clicked on letter', element.id);
+      let letterElement = document.querySelector(`.letter-${element.id}`);
+      letterElement.classList.toggle('visible');
+
+  });
+});
