e7d66df39d03c5853abe3c47a16fc9a0a5bac803
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-04-11 20:13:44 -0400

    push

--- activities/api/script.js
+++ activities/api/script.js
@@ -2,5 +2,38 @@ let news;
 
 function getNews(){
     fetch('https://apilist.fun/api/news')
-    
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      factList = data; // store data
+      displayFact(0);
+    })
+    .catch(error => console.log(error));
 }
+
+function displayFact(factIndex){
+  let fact = factList[factIndex];
+  let text = fact.text;
+  document.getElementById('fact').innerHTML = text;
+}  
+
+window.onload = function(e){ 
+
+    let factCount = 0;
+
+    getFact();
+
+    let refreshButton = document.getElementById('refresh');
+    refreshButton.addEventListener('click', function(){
+
+     if(factCount < 4){        
+        factCount ++; // increment fact index until 5
+        displayFact(factCount);
+      }else{
+        factCount = 0; // reset fact index
+        getFact();
+      }  
+       
+    });
+
+};
