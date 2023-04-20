9c06b2c26100bb8d2c761ef2e690141750ae060c
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-04-20 18:15:41 -0400

    push

--- projects/project3/script.js
+++ projects/project3/script.js
@@ -1,12 +1,53 @@
-// Get references to the clickable sentence and the hidden sentence
-const clickMe = document.getElementById("click-me");
-const hiddenSentence = document.getElementById("hidden-sentence");
 
-// Add an event listener to the clickable sentence
-clickMe.addEventListener("click", function() {
-  // Update the content of the hidden sentence and show it
-  hiddenSentence.textContent = "This is the hidden sentence that will appear when you click the first sentence.";
-  hiddenSentence.style.display = "block";
-});
-
- 
+function toggle() {
+    var sentence1 = document.getElementById("sentence1");
+    var sentence2 = document.getElementById("sentence2");
+    var sentence3 = document.getElementById("sentence3");
+    var sentence4 = document.getElementById("sentence4");
+    var sentence5 = document.getElementById("sentence5");
+    var sentence6 = document.getElementById("sentence6");
+    var sentence7 = document.getElementById("sentence7");
+    var sentence8 = document.getElementById("sentence8");
+    var sentence9 = document.getElementById("sentence9");
+    var sentence10 = document.getElementById("sentence10");
+    var sentence11 = document.getElementById("sentence11");
+    var sentence12 = document.getElementById("sentence12");
+    var sentence13 = document.getElementById("sentence13");
+    var sentence14 = document.getElementById("sentence14");
+    var sentence15 = document.getElementById("sentence15");
+    if (sentence1.style.display === "none") {
+      sentence1.style.display = "block";
+      sentence2.style.display = "block";
+      sentence3.style.display = "block";
+      sentence4.style.display = "block";
+      sentence5.style.display = "block";
+      sentence6.style.display = "block";
+      sentence7.style.display = "block";
+      sentence8.style.display = "block";
+      sentence9.style.display = "block";
+      sentence10.style.display = "block";
+      sentence11.style.display = "block";
+      sentence12.style.display = "block";
+      sentence13.style.display = "block";
+      sentence14.style.display = "block";
+      sentence15.style.display = "block";
+    } else {
+      sentence1.style.display = "none";
+      sentence2.style.display = "none";
+      sentence3.style.display = "none";
+      sentence4.style.display = "none";
+      sentence5.style.display = "none";
+      sentence6.style.display = "none";
+      sentence7.style.display = "none";
+      sentence8.style.display = "none";
+      sentence9.style.display = "none";
+      sentence10.style.display = "none";
+      sentence11.style.display = "none";
+      sentence12.style.display = "none";
+      sentence13.style.display = "none";
+      sentence14.style.display = "none";
+      sentence15.style.display = "none";
+    }
+  }
+  
+  
