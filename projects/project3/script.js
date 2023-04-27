cabffb78ff48fdd9b2165753bdf6609cb094b7b0
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-04-27 19:59:37 -0400

    push undefined events

--- projects/project3/script.js
+++ projects/project3/script.js
@@ -1,16 +1,18 @@
 
 
-
             function filterEvents() {
-            
+                console.log('filter events');
               fetch('https://data.cityofnewyork.us/resource/6v4b-5gp4.json')
                 .then(response => response.json())
                 .then(data => {
-                
+                 console.log(data);
                   const filteredData = data.filter(event => {
-                    const year = new Date(event.date).getFullYear();
+                    // console.log(event['date_and_time']);
+                    const year = new Date(event['date_and_time']).getFullYear();
                     return year >= 2017 && year <= 2021;
                   });
+
+                  console.log(filteredData);
                
                   const eventsDiv = document.getElementById('events');
                   eventsDiv.innerHTML = '';
@@ -18,8 +20,8 @@
                     const eventElement = document.createElement('div');
                     eventElement.className = `event event-year-${new Date(event.date).getFullYear()}`;
                     eventElement.innerHTML = `
-                      <h2>${event.name}</h2>
-                      <p>Date: ${event.date}</p>
+                      <h2>${event.event_name}</h2>
+                      <p>Borough: ${event.borough}</p>
                       <p>Location: ${event.location}</p>
                     `;
                     eventsDiv.appendChild(eventElement);
