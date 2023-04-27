ed4009eca24e962b5354578f55edcfdc1fb0a5cd
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-04-27 19:21:08 -0400

    push

--- projects/project3/script.js
+++ projects/project3/script.js
@@ -1,60 +1,28 @@
-const url = 'https://data.cityofnewyork.us/resource/6v4b-5gp4.json';
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('data');
-    let html = '';
 
-    data.forEach(item => {
-      html += `<div>${item.location}</div>`;
-      // add more HTML elements here to display other data
-    });
 
-    container.innerHTML = html;
-  });
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('data');
-    let html = '';
-
-    data.forEach(item => {
-      html += `
-        <div>
-          <p>Name: ${item.location}</p>
-          <p>Audience: ${item.audience}</p>
-          <p>Audience: ${item.date_and_time}</p>
-          <p>Audience: ${item.attendance}</p>
-          <p>Audience:${item.event_name}</p>
-          <p>Audience:${item.event_type}</p>
-        </div>
-      `;
-    });
-
-    container.innerHTML = html;
-  });
-
-  
-  
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('data');
-    let html = '';
-
-    data.forEach((item, index) => {
-      const colorClass = index % 3 === 2 ? 'one' : 'two';
-      html += `
-        <div class="${colorClass}">
-        <p>Audience: ${item.audience}</p>
-          <p>Audience: ${item.date_and_time}</p>
-          <p>Audience: ${item.attendance}</p>
-          <p>Audience:${item.event_name}</p>
-          <p>Audience:${item.event_type}</p>
-        </div>
-      `;
-    });
-
-    container.innerHTML = html;
-  });
+            function filterEvents() {
+            
+              fetch('https://data.cityofnewyork.us/resource/6v4b-5gp4.json')
+                .then(response => response.json())
+                .then(data => {
+                
+                  const filteredData = data.filter(event => {
+                    const year = new Date(event.date).getFullYear();
+                    return year >= 2017 && year <= 2021;
+                  });
+               
+                  const eventsDiv = document.getElementById('events');
+                  eventsDiv.innerHTML = '';
+                  filteredData.forEach(event => {
+                    const eventElement = document.createElement('div');
+                    eventElement.className = `event event-year-${new Date(event.date).getFullYear()}`;
+                    eventElement.innerHTML = `
+                      <h2>${event.name}</h2>
+                      <p>Date: ${event.date}</p>
+                      <p>Location: ${event.location}</p>
+                    `;
+                    eventsDiv.appendChild(eventElement);
+                  });
+                });
+            }
