ab2354b55fc04f65ee16f768d0b1af0972bb29ca
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-04-27 21:05:45 -0400

    creating search bar

--- projects/project3/script.js
+++ projects/project3/script.js
@@ -30,3 +30,100 @@
                   });
                 });
             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
+            fetch('https://data.cityofnewyork.us/resource/6v4b-5gp4.json')
+            .then(response => response.json())
+            .then(data => {
+
+            
+
+            });
+            function filterData(category) {
+                
+
+                const filteredData = data.filter(event => event.category === category);
+              
+                return filteredData;
+              }
+              const select = document.getElementById('category');
+             
+              select.addEventListener('change', () => {
+                const category = select.value;
+                const filteredData = filterData(category);
+              
+                
+              });
+                                      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        
+              
+              // Load data from the URL
+const url = 'https://data.cityofnewyork.us/resource/6v4b-5gp4.json';
+let data;
+
+fetch(url)
+  .then(response => response.json())
+  .then(json => {
+    data = json;
+  });
+
+// Filter data based on selected category and search query
+function filterData() {
+  const searchBar = document.getElementById('searchBar');
+  const categorySelect = document.getElementById('categorySelect');
+  const searchTerm = searchBar.value.toLowerCase();
+  const selectedCategory = categorySelect.value;
+  const filteredData = data.filter(item => {
+    return item[selectedCategory].toLowerCase().includes(searchTerm);
+  });
+
+  // Display filtered data on the HTML page
+  const dataBody = document.getElementById('dataBody');
+  dataBody.innerHTML = '';
+  filteredData.forEach(item => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${item.unique_key}</td>
+      <td>${item.created_date}</td>
+      <td>${item.complaint_type}</td>
+      <td>${item.category}</td>
+      <td>${item.location}</td>
+      <td>${item.borough}</td>
+      <td>${item.attendance}</td>
+      <td>${item.resolution_description}</td>
+      <!-- Add more columns as needed -->
+    `;
+    dataBody.appendChild(row);
+  });
+}
+
